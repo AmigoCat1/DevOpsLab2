1541d54a6f51e0e52ca13be550377120e7c2703e
AmigoCat1 <barmalyy@gmail.com>
2024-10-23 18:48:11 +0300

    Hotfix:calculate sum of first 3 elements

--- cpp/funcA.cpp
+++ cpp/funcA.cpp
@@ -1,5 +1,11 @@
 #include "FuncA.h"
+#include <cmath>
 
 double FuncA::calculateFuncA() {
-	return 0;
+	double sum = 1.0;
+	double x = 1.0;
+	for (int i = 1; i <= 3; ++i) {
+		sum += std::pow(x, 2 * i) / std::tgamma(2 * i + 1);
+	}
+	return sum;
 }
