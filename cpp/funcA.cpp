a01c0292f61c6c73d97ebd366739df86f3025db4
AmigoCat1 <barmalyy@gmail.com>
2024-10-23 18:54:51 +0300

    Updated FuncA to calculate n elements, added comments

--- cpp/funcA.cpp
+++ cpp/funcA.cpp
@@ -1,5 +1,12 @@
 #include "FuncA.h"
+#include <cmath>
 
-double FuncA::calculateFuncA() {
-	return 0;
+//Function calculate sum first n elements
+double FuncA::calculateFuncA(int n) {
+	double sum = 1.0;
+	double x = 1.0;
+	for (int i = 1; i <= n; ++i) {
+		sum += std::pow(x, 2 * i) / std::tgamma(2 * i + 1);
+	}
+	return sum;
 }
